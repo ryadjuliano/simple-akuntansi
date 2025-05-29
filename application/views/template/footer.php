03e707593898ca59b4b3c25bff1268e74fb61b17
ryadjuliano <ryadjuliano@gmail.com>
2025-05-29 20:23:45 +0700

    Update footer.php

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -7,8 +7,6 @@
   
   <script src="<?= base_url('assets/js/script.js') ?>"></script>
   <script src="<?= base_url('assets/js/plugins/smooth-scrollbar.min.js') ?>"></script>
-  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/css/select2.min.css" rel="stylesheet" />
-  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/js/select2.min.js"></script>
   <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <!-- Bootstrap Datepicker CSS -->
@@ -28,8 +26,8 @@
     }
   </script>
   <!-- Github buttons -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<!-- <script async defer src="https://buttons.github.io/buttons.js"></script> -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> -->
 <script src="<?= base_url('assets/js/argon-dashboard.min.js?v=2.1.0') ?>"></script>
 
 </body>
